94a37fbb8d3e53c6605f82d6f5637d6e95b4384d
flavioflnlima <flavioflnlima@gmail.com>
2020-06-08 23:15:44 -0300

    feat: Criada as entidades

---
+++ QuickBuy.Dominio/ObjetoValor/FormaPagamento.cs
@@ -0,0 +1,27 @@
+﻿using QuickBuy.Dominio.Enumerados;
+
+namespace QuickBuy.Dominio.ObjetoValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool EhBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCretido; }
+        }
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
